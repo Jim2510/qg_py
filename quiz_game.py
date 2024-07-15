10f69e567e70bf79ffa48c3a2164863be090ec63
Jim2510 <gmguglielmino@gmail.com>
2024-07-15 11:52:20 +0200

    quiz game first version

---
+++ quiz_game.py
@@ -0,0 +1,7 @@
+print("Welcome to Python Quiz Game! \n This is a simple program made by me to help me study Python tracking a score wich I can use to improve my skills. \n")
+
+playing = input("Are you ready? (y/n) ")
+
+if playing.lower() != 'y':
+    print("Ok, take your time")
+    quit()
